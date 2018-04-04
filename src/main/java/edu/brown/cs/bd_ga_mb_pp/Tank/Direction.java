3e585adccf698a3ec5ebc30271821064a8c675eb
Prithu Dasgupta <prithudasgupta@prithus-air.devices.brown.edu>
2018-04-03 22:21:50 -0400

    Tank code 1

---
+++ src/main/java/edu/brown/cs/bd_ga_mb_pp/Tank/Direction.java
@@ -0,0 +1,7 @@
+package edu.brown.cs.bd_ga_mb_pp.Tank;
+
+public enum Direction {
+	
+	LEFT, RIGHT, FORWARD, BACKWARD;
+
+}
---
+++ src/main/java/edu/brown/cs/bd_ga_mb_pp/Tank/UserTank.java
@@ -0,0 +1,63 @@
+package edu.brown.cs.bd_ga_mb_pp.Tank;
+
+import edu.brown.cs.bd_ga_mb_pp.Collect.Angle;
+import edu.brown.cs.bd_ga_mb_pp.Collect.Coordinate;
+
+public class UserTank implements Tank{
+	
+	private Coordinate location;
+	private Angle angleForward;
+	private Angle launcherAngle;
+	private boolean isAlive;
+	
+	private static final double MOVE_SPEED = .01;
+	private static final double ROTATE_SPEED = .01;
+	
+	public UserTank(Coordinate startCoord, double startDegrees) {
+		location = startCoord;
+		angleForward = new Angle(startDegrees, ROTATE_SPEED);
+		launcherAngle = new Angle(startDegrees, ROTATE_SPEED);
+		isAlive = true;
+	}
+	
+
+	@Override
+	public void move(Direction d) {
+		if (d.equals(Direction.FORWARD)) {
+			
+		}
+		else if (d.equals(Direction.BACKWARD)) {
+			
+		}	
+	}
+
+	@Override
+	public void rotateTank(Direction d) {
+		if (d.equals(Direction.LEFT)) {
+			angleForward.rotateCounterClockwise();
+			
+		}
+		else if (d.equals(Direction.RIGHT)) {
+			angleForward.rotateClockwise();
+		}
+		
+	}
+
+	@Override
+	public void rotateLauncher(Direction d) {
+		if (d.equals(Direction.LEFT)) {
+			
+		}
+		else if (d.equals(Direction.RIGHT)) {
+			
+		}			
+	}
+
+	@Override
+	public void shoot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
