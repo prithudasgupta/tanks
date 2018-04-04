4995eb4947f51faf5be7a47609182e67db50c46c
Ben Deckey <BDeckey@bens-mbp-3.devices.brown.edu>
2018-04-03 21:13:44 -0400

    fixed main

--- src/main/java/edu/brown/cs/bdeckey/Main/Main.java
+++ src/main/java/edu/brown/cs/bdeckey/Main/Main.java
@@ -4,27 +4,15 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Map;
 
-
-import edu.brown.cs.bdeckey.Autocorrect.CorrectionHandler;
-
-import edu.brown.cs.bdeckey.Bacon.ActorHandler;
-import edu.brown.cs.bdeckey.Bacon.ConnectionHandler;
-import edu.brown.cs.bdeckey.Bacon.DropDownHandler;
-import edu.brown.cs.bdeckey.Bacon.MovieHandler;
-import edu.brown.cs.bdeckey.stars.StarsHandler;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import spark.ExceptionHandler;
-import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
-import spark.TemplateViewRoute;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import com.google.common.collect.ImmutableMap;
 
 import freemarker.template.Configuration;
 
@@ -48,7 +36,6 @@ public final class Main {
   }
 
   private String[] args;
-  private Repl repl;
   private boolean gui = false;
 
   private Main(String[] args) {
@@ -63,7 +50,7 @@ public final class Main {
     OptionSet options = parser.parse(args);
     // This is initialized before the GUI because the GUI will use it for its
     // information about the tree.
-    repl = new Repl();
+
 
     if (options.has("gui")) {
       runSparkServer((int) options.valueOf("port"));
@@ -72,7 +59,6 @@ public final class Main {
 
 
     // Process commands in a REPL
-    repl.run(gui);
     // end GUI
     if (options.has("gui")) {
       Spark.stop();
@@ -97,88 +83,9 @@ public final class Main {
 
     FreeMarkerEngine freeMarker = createEngine();
 
-    // Setup Spark Routes
-    Spark.get("/stars", new FrontHandler(), freeMarker);
 
-    // Post results of search
-    Spark.post("/results", new StarsHandler(repl), freeMarker);
-
-    // Autocorrect Page
-    Spark.get("/autocorrect", new AutoFrontHandler(), freeMarker);
-
-    // Endpoint which manages the suggestions
-    Spark.post("/suggestions", new CorrectionHandler(repl));
-
-    // Home page
-    Spark.get("/home", new HomeHandler(), freeMarker);
-
-    // Bacon Page
-    Spark.get("/connect", new BaconFrontHandlor(), freeMarker);
-
-    // Endpoitn which manages the bacon path
-    Spark.post("/connections", new ConnectionHandler(repl));
-
-    // Actor Pages
-    Spark.get("/actor/:actor", new ActorHandler(repl), freeMarker);
-
-    // Movie Pages
-    Spark.get("/movie/:movie", new MovieHandler(repl), freeMarker);
-
-    // DropDown Suggestions
-    Spark.post("/dropdown", new DropDownHandler(repl));
   }
 
-  /**
-   * Handle requests to the front page of Bacon website.
-   */
-  private static class BaconFrontHandlor implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of("title",
-              "Bacon: Connect");
-      return new ModelAndView(variables, "connect.ftl");
-    }
-  }
-
-  /**
-   * Handle requests to the front page of our Stars website.
-   *
-   * @author jj
-   */
-  private static class FrontHandler implements TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of("title",
-          "Stars: Query the database");
-      return new ModelAndView(variables, "query.ftl");
-    }
-  }
-
-  /**
-   * Handle requests to the front page of the Autocorrect website.
-   *
-   */
-  private static class AutoFrontHandler implements  TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of("title",
-              "Corrector");
-      return new ModelAndView(variables, "corrector.ftl");
-    }
-  }
-
-  /**
-   * Handle requests to the front page of the Autocorrect website.
-   *
-   */
-  private static class HomeHandler implements  TemplateViewRoute {
-    @Override
-    public ModelAndView handle(Request req, Response res) {
-      Map<String, Object> variables = ImmutableMap.of("title",
-              "Home");
-      return new ModelAndView(variables, "home.ftl");
-    }
-  }
 
   /**
    * Display an error page when an exception occurs in the server.
