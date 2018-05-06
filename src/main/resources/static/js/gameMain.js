7d7bc9d2fe8ad9f53b2f1a0b85528874cb1d9b22
michael bardakji <michael_bardakji@brown.edu>
2018-05-06 01:17:57 -0400

    need to pull '

--- src/main/resources/static/js/gameMain.js
+++ src/main/resources/static/js/gameMain.js
@@ -755,7 +755,7 @@ function updateBullet() {
                     }
                 }
                 if (bullet.sprite.collidesWith(user)) {
-                    isGameOver = true;
+                    //isGameOver = true;
                 }
                 if (!collided) {
                     bullet.sprite.update();
@@ -857,8 +857,8 @@ function getBorderingLandTiles(xCoord, yCoord){
 }
 
 function checkEndGame() {
-    return (statEnemies.length === 0 && dumbEnemies.length === 0 &&
-        pathEnemies.length === 0 && homingEnemies.length === 0 && allEnemies.length === 0);
+    return false ;/*(statEnemies.length === 0 && dumbEnemies.length === 0 &&
+        pathEnemies.length === 0 && homingEnemies.length === 0 && allEnemies.length === 0);*/
 }
 
 function homingHelper(movingEnemy) {
